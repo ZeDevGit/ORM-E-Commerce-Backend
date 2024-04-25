f302b0849852bc441dac6a6524f889e3565462eb
Ze <elawrence.bda@gmail.com>
2024-04-24 22:27:53 -0400

    Added seeds and fixed routing

---
+++ seeds/category-seeds.js
@@ -0,0 +1,23 @@
+const { category } = require('../models');
+
+const categoryData = [
+    {
+        category_name: 'Shirts',
+    },
+    {
+        category_name: 'Shorts',
+    },
+    {
+        category_name: 'Music',
+    },
+    {
+        category_name: 'Hats',
+    },
+    {
+        category_name: 'Shoes',
+    },
+];
+
+const seedCategories = () => category.bulkCreate(categoryData);
+
+module.exports = seedCategories;
